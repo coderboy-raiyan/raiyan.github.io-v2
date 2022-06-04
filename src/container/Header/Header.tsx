88197a0655c5e2af635516d06ff6fa3c68c2ddce
Raiyan <raiyan5764@gmail.com>
2022-06-04 16:47:32 +0600

    [ADD] header section done

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,16 +1,30 @@
+/* eslint-disable react/no-array-index-key */
 import { motion } from "framer-motion";
+import Image from "next/image";
 import React from "react";
+import { images } from "../../constants";
 import styles from "./Header.module.scss";
+
+const scaleVariants = {
+    whileInView: {
+        scale: [0, 1],
+        opacity: [0, 1],
+        transition: {
+            duration: 1,
+            ease: "easeInOut",
+        },
+    },
+};
 
 function Header() {
     return (
-        <div className={`${styles.app__header} ${styles.app__flex}`}>
+        <div className={`${styles.app__header} app__flex`}>
             <motion.div
                 whileInView={{ x: [-100, 0], opacity: [0, 1] }}
                 transition={{ duration: 0.5 }}
                 className={`${styles.app__header_info}`}
             >
-                <div className={`${styles.app___header_badge}`}>
+                <div className={`${styles.app__header_badge}`}>
                     <div className={`${styles.badge_cmp} app__flex`}>
                         <span>👋</span>
                         <div style={{ marginLeft: 20 }}>
@@ -18,12 +32,39 @@ function Header() {
                             <h1 className="head-text">Raiyan</h1>
                         </div>
                     </div>
-                </div>
 
-                <div className={`${styles.tag_cmp} app__flex`}>
-                    <p className="p-text">Web Developer</p>
-                    <p className="p-text">Freelancer</p>
+                    <div className={`${styles.tag_cmp} app__flex`}>
+                        <p className="p-text">Web Developer</p>
+                        <p className="p-text">Freelancer</p>
+                    </div>
                 </div>
+            </motion.div>
+
+            <motion.div
+                whileInView={{ opacity: [0, 1] }}
+                transition={{ duration: 0.5, delayChildren: 0.5 }}
+                className={`${styles.app__header_img}`}
+            >
+                <Image src={images.profile} alt="profile_bg" />
+                <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    transition={{ duration: 1, ease: "easeInOut" }}
+                    className={`${styles.overlay_circle}`}
+                >
+                    <Image src={images.circle} />
+                </motion.div>
+            </motion.div>
+
+            <motion.div
+                variants={scaleVariants}
+                whileInView={scaleVariants.whileInView}
+                className={`${styles.app__header_circles}`}
+            >
+                {[images.flutter, images.redux, images.sass].map((circle, index) => (
+                    <div className={`${styles.circle_cmp} app__flex`} key={`circle-${index}`}>
+                        <Image src={circle} alt="profile_bg" />
+                    </div>
+                ))}
             </motion.div>
         </div>
     );
