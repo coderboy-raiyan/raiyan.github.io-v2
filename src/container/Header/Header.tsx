e4ae4017a47abd7f5a927b54748ae03550093d46
Raiyan <raiyan5764@gmail.com>
2022-06-04 14:22:47 +0600

    half header done

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,8 +1,32 @@
+import { motion } from "framer-motion";
 import React from "react";
 import styles from "./Header.module.scss";
 
 function Header() {
-    return <div className={styles.app}>Header</div>;
+    return (
+        <div className={`${styles.app__header} ${styles.app__flex}`}>
+            <motion.div
+                whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+                transition={{ duration: 0.5 }}
+                className={`${styles.app__header_info}`}
+            >
+                <div className={`${styles.app___header_badge}`}>
+                    <div className={`${styles.badge_cmp} app__flex`}>
+                        <span>👋</span>
+                        <div style={{ marginLeft: 20 }}>
+                            <p className="p-text">Hello, I am</p>
+                            <h1 className="head-text">Raiyan</h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={`${styles.tag_cmp} app__flex`}>
+                    <p className="p-text">Web Developer</p>
+                    <p className="p-text">Freelancer</p>
+                </div>
+            </motion.div>
+        </div>
+    );
 }
 
 export default Header;
